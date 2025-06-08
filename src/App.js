60870422b4000ecf2bacd6073239cc386f0a3960
Ram Kumar <ramkumarramar2237@gmail.com>
2025-06-08 21:59:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React  from 'react';
 
 import SignIn from './Sign/SignIn';
 import SignUp from './Sign/SignUp';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 
